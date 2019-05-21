3617532c2b2b87edfa82d787bdb0091671626b71
AlissonWeiss <alissonweiss@id.uff.br>
2019-05-20 22:16:28 -0300

    first commit

---
+++ Escalonador-SO/src/escalonador/so/EscalonadorSO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escalonador.so;
+/**
+ *
+ * @author aliss
+ */
+public class EscalonadorSO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
